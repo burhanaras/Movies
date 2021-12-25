260aaa324770d6b090c683081bf7f9e7d5efbdb5
Burhan Aras <burhanaras@live.com>
2021-12-25 20:21:16 +0300

    Data transfer objects and model objects has been created.

---
+++ Pixel_Perfect_SwiftUI/Pixel_Perfect_SwiftUI/Model/Data Transfer Objects/MoviesResponse.swift
@@ -0,0 +1,24 @@
+//
+//  MoviesResponse.swift
+//  Pixel_Perfect_SwiftUI
+//
+//  Created by Burhan Aras on 25.12.2021.
+//
+
+import Foundation
+
+struct MovieDTO: Codable {
+    let id: Int
+    let title: String
+    let backdrop_path: String?
+    let poster_path: String?
+    let overview: String
+    let vote_average: Double
+    let release_date: String?
+    
+}
+
+struct MoviesResponse: Codable {
+    let page: Int
+    let results: [MovieDTO]
+}
---
+++ Pixel_Perfect_SwiftUI/Pixel_Perfect_SwiftUI/Model/Model Objects/Movie.swift
@@ -0,0 +1,66 @@
+//
+//  Movie.swift
+//  Pixel_Perfect_SwiftUI
+//
+//  Created by Burhan Aras on 25.12.2021.
+//
+
+import Foundation
+
+
+struct Movie: Identifiable {
+    let id: Int
+    let title: String
+    let backdropURL: URL
+    let posterURL: URL
+    let releaseDate: String
+    let rating: String
+}
+
+
+extension Movie {
+    static func fromDTO(dto: MovieDTO) -> Movie {
+        
+        let dateFormatter: DateFormatter = {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-mm-dd"
+            return dateFormatter
+        }()
+        
+        let dateFormatterWithDot: DateFormatter = {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-mm-dd"
+            return dateFormatter
+        }()
+        
+        let yearFormatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy"
+            return formatter
+        }()
+        
+        var yearText: String {
+            guard let releaseDate = dto.release_date, let date = dateFormatter.date(from: releaseDate) else {
+                return "n/a"
+            }
+            return yearFormatter.string(from: date)
+        }
+        
+        var backdropURL: URL {
+            return URL(string: "https://image.tmdb.org/t/p/w500\(dto.backdrop_path ?? "")")!
+        }
+        
+        var posterURL: URL {
+            return URL(string: "https://image.tmdb.org/t/p/w500\(dto.poster_path ?? "")")!
+        }
+        
+        var releaseDate: String {
+            guard let releaseDate = dto.release_date, let date = dateFormatter.date(from: releaseDate) else {
+                return "n/a"
+            }
+            return dateFormatterWithDot.string(from: date)
+        }
+        
+        return Movie(id: dto.id, title: "\(dto.title) (\(yearText)", backdropURL: backdropURL, posterURL: posterURL, releaseDate: releaseDate, rating: "\(dto.vote_average)/10")
+    }
+}
