8d9663d79507111fab6893518cbc82b1af447a95
Burhan Aras <burhanaras@live.com>
2021-12-25 20:09:19 +0300

    Initial commit: Bismillah

---
+++ Pixel_Perfect_SwiftUI/Pixel_Perfect_SwiftUI/Pixel_Perfect_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pixel_Perfect_SwiftUIApp.swift
+//  Pixel_Perfect_SwiftUI
+//
+//  Created by Burhan Aras on 25.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct Pixel_Perfect_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
